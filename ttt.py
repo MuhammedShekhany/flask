403b87610791d384c7bdf0bd709d092f10f92113
MuhammedShekhany <teshksss@gmail.com>
2024-05-29 22:40:45 +0300

    initial commit

---
+++ ttt.py
@@ -0,0 +1,18 @@
+from binance.client import Client
+from binance.exceptions import BinanceAPIException
+from binance.exceptions import BinanceRequestException
+import time
+# Replace with your Binance API key and secret
+api_key = 'HMO6jdvndrJW6ZPywUndhrimAzT8EnT0q8CGIoqt2PJNFxZiKaLKTdlNk9MLRhpi'
+api_secret = 'rpKjZPz4zbyheueA7ImeRgc2DbFkGhOf1DKaeQeoRsqHLpuybqPzOVj6ALdVb450'
+
+        # Create a Binance client
+client = Client(api_key, api_secret)
+client.tld='us'
+
+client.API_URL = 'https://api.binance.com/api'
+server_time = client.get_server_time()
+client.timestamp_offset = server_time['serverTime'] - int(time.time() * 1000)            
+usdt_account_info =  client.get_asset_balance(asset='USDT')
+print("Spot Account Information usdt:")
+print(usdt_account_info['free'])
