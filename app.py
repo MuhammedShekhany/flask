467a55bdf1c3d1c9b0393bf38e38a85c812e752f
MuhammedShekhany <teshksss@gmail.com>
2024-06-03 00:22:36 +0300

    Your commit message here

--- app.py
+++ app.py
@@ -52,10 +52,10 @@ def handle_post():
                 last_price = float(ticker['price'])
                 print('lastprice:')
                 print(last_price)
-                stop_price =round(last_price -(0.003 * last_price),4)  # 0.5% below the last price
+                stop_price =round(last_price -(0.005 * last_price),4)  # 0.5% below the last price
                 print('stop_price:')
                 print(stop_price)
-                take_profit_price =round(last_price+(0.005 * last_price),4)  # 0.5% above the last price
+                take_profit_price =round(last_price+(0.01 * last_price),4)  # 0.5% above the last price
                 print('take_profit_price:')
                 print(take_profit_price)
                 stop_limit_time_in_force = 'GTC'
